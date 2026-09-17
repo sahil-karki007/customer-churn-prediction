6862d27c345be41b50c959b48600fbfa92e83536
sahi karki <karkisahil2003@gmail.com>
2026-09-17 12:26:52 +0530

    Point frontend to deployed Render backend URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -40,7 +40,7 @@ function App() {
     setResult(null);
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/predict', {
+      const response = await fetch('https://customer-churn-prediction-vuhi.onrender.com/predict', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
